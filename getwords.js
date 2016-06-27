54e3413395502d60bd6d85345b02bc0cff36295c
dmedrest <whydiff@gmail.com>
2016-06-27 09:06:51 +0800

    Create getwords.js

---
+++ getwords.js
@@ -0,0 +1,6 @@
+var arr = $('#yolo').getElementsByTagName('tr'),
+    res = '';
+for (var i = 0; i < arr.length; i++) {
+    var td = arr[i].getElementsByTagName('td');
+    if (td.length > 1) res += td[1].textContent
+}
