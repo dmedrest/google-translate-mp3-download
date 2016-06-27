97504d40080bbe665c20e933edba31f09f753cbc
dmedrest <whydiff@gmail.com>
2016-06-27 09:07:58 +0800

    Update getwords.js

--- getwords.js
+++ getwords.js
@@ -2,5 +2,5 @@ var arr = $('#yolo').getElementsByTagName('tr'),
     res = '';
 for (var i = 0; i < arr.length; i++) {
     var td = arr[i].getElementsByTagName('td');
-    if (td.length > 1) res += td[1].textContent
+    if (td.length > 1) res += (',' + td[1].textContent);
 }
